6a1d77771fd2716850d4c92a30486df05a981495
sayefdeen <sayefdeen.alrawad@gmail.com>
2020-07-08 15:16:09 +0300

    add all folders to Repo

---
+++ Aboutme.js
@@ -0,0 +1,99 @@
+"use strict";
+
+var answer1, answer6, answer7;
+var finalResult = 0;
+
+var userName = prompt("Hello Stranger Can I Know Your Name?");
+alert("Nice To meet You, " + userName);
+questions();
+alert(
+  "Thank You for you Time, " +
+    userName +
+    " your final result is " +
+    finalResult +
+    " of 7"
+);
+
+function questions() {
+  alert("I will ask you some yes/no questions is that ok " + userName + " ?");
+  var questionArray = [
+    "Question 1 \n Did I graduated as a civil Engineer?",
+    "Question 2 \n Did I graduated at 2017?",
+    "Question 3 \n Is Java on of my skills?",
+    "Question 4 \n Is it true thats my name is sayefdeen?",
+    "Question 5 \n Is it true that i graduated with an excellent rating?",
+  ];
+  for (var q = 0; q < questionArray.length; q++) {
+    console.log(finalResult);
+    answer1 = prompt(questionArray[q]);
+    while (
+      answer1.toLowerCase() !== "yes" &&
+      answer1.toLowerCase() !== "y" &&
+      answer1.toLowerCase() !== "no" &&
+      answer1.toLowerCase() !== "n"
+    ) {
+      answer1 = prompt(
+        "You have to answer with y/n OR yes/no \n " + questionArray[q]
+      );
+    }
+    var guess = correct(answer1);
+    alert(guess);
+    console.log("You answer to " + questionArray[q] + " was " + guess);
+  }
+
+  // Should have a number validation in this section.
+  for (var i = 4; i > 0; i--) {
+    var rightAnswer = Math.floor(Math.random() * 11);
+    console.log(rightAnswer);
+    answer6 = prompt(
+      "You have " +
+        i +
+        " chance left \n what is the number I have in mind between 0-10"
+    );
+    if (answer6 > rightAnswer) {
+      alert("Too High");
+    } else if (answer6 < rightAnswer) {
+      alert("Too Low");
+    } else {
+      finalResult++;
+      alert("Correct");
+      break;
+    }
+  }
+  console.log(finalResult);
+  if (i == 0) alert("The correct number was " + rightAnswer);
+
+  var topMoviesArr = ["Predestination", "Shutter Island", "Mr Nobody"];
+
+  for (var j = 6; j > 0; j--) {
+    answer7 = prompt(
+      "You have " +
+        j +
+        " chances left \n Can you guess one of my top 3 favorite movies"
+    ).toLowerCase();
+    if (answer7 == topMoviesArr[0].toLowerCase()) {
+      finalResult++;
+      alert("WOW! Thats My number One Movie");
+      break;
+    } else if (answer7 == topMoviesArr[1].toLowerCase()) {
+      finalResult++;
+      alert("WOW! Thats My Second Movie");
+      break;
+    } else if (answer7 == topMoviesArr[2].toLowerCase()) {
+      finalResult++;
+      alert("WOW! Thats My Third Movie");
+      break;
+    } else {
+      alert("You Got this Wrong!!");
+    }
+  }
+}
+
+function correct(answer) {
+  if (answer.toLowerCase() === "yes" || answer.toLowerCase() === "y") {
+    finalResult++;
+    return "Correct";
+  } else {
+    return "Wrong";
+  }
+}
